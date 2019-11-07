4ad58c1a658ff56e1590042224f3fe20eec2e242
Le Nhan <nhanletrong999@gmail.com>
2019-11-08 02:29:38 +0700

    Finish add template

--- Ecommerce Web/src/java/bll/ProductBLL.java
+++ Ecommerce Web/src/java/bll/ProductBLL.java
@@ -1,14 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bll;
 
-/**
- *
- * @author ADMIN
- */
+import entity.Product;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+
 public class ProductBLL {
-    
+    public List<Product> getNewProducts (int number) {
+        try{
+            Context initContext = new InitialContext();
+            DataSource ds = (DataSource) initContext.lookup("java:comp/env/jdbc/eMarket");
+            Connection conn = ds.getConnection();
+            Statement sttm = conn.createStatement();
+            String sql = "select * from Product";
+            ResultSet rs = sttm.executeQuery(sql);
+            ArrayList<Product> prods = new ArrayList<>();
+            while(rs.next()){
+                Product p = new Product();
+                p.setProductID(rs.getInt("product_id"));
+                p.setCategoryID(rs.getInt("category_id"));
+                p.setPrice(rs.getFloat("price"));
+                p.setDescription(rs.getString("description"));
+                p.setImage(rs.getString("image"));
+                p.setName(rs.getString("name"));
+                p.setThumbImage(rs.getString("thumb_image"));
+                p.setLastUpdate(rs.getDate("last_update"));
+                p.setDescriptionDetail(rs.getString("description_detail"));
+                prods.add(p);   
+            }
+            return prods;
+        } catch(SQLException ex){
+        } catch (NamingException ex) {
+            Logger.getLogger(ProductBLL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
 }
