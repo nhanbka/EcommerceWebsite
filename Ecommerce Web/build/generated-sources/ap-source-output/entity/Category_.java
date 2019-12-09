72b4c1745e82030fa63c34e5f04485f2834438b7
NhanBKA <nhanletrong999@gmail.com>
2019-12-09 19:55:17 +0700

    Add to finish config .gitignore

--- Ecommerce Web/build/generated-sources/ap-source-output/entity/Category_.java
+++ Ecommerce Web/build/generated-sources/ap-source-output/entity/Category_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-03T17:26:57")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-09T01:14:58")
 @StaticMetamodel(Category.class)
 public class Category_ { 
 
