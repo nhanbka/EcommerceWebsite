4673cab5c72b65a171d123401703543ab78cda80
TakarinTD <56709998+TakarinTD@users.noreply.github.com>
2019-12-10 22:16:59 +0700

    logout

--- Ecommerce Web/src/java/controller/Register.java
+++ Ecommerce Web/src/java/controller/Register.java
@@ -86,7 +86,7 @@ public class Register extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+        request.setCharacterEncoding("UTF-8");
         Context initContext;
         try {
             initContext = new InitialContext();
